8c2b5b3f820536e499cc25eda6e96f1d526a2ee6
josedominic <josudominic007@gmail.com>
2020-07-12 21:02:41 +0530

    search added

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>",views.show_wiki,name="show_wiki")
+    path("wiki/<str:title>",views.show_wiki,name="show_wiki"),
+    path("search/",views.search,name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponseNotFound
+from django.http import HttpResponseNotFound,HttpResponseRedirect
+from django import forms
+from django.urls import reverse
 
 from . import util
 
 from markdown2 import markdown
+import re
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "title":'Encyclopedia'
     })
 
 def show_wiki(request,title):
@@ -18,3 +22,22 @@ def show_wiki(request,title):
             "title":title
             })
     return HttpResponseNotFound('<h1>Page Not Found </h1>')
+
+def search(request):
+    query = request.GET['q']
+    entries = util.list_entries()
+    #query = query.lower()
+    results = []
+    for e in entries:
+        if (re.search(query,e,re.IGNORECASE)):
+            results.append(e)
+    if len(results)==1 and results[0]==query:
+        return HttpResponseRedirect(reverse('show_wiki',kwargs={'title':query}))
+    else:
+        return render(request,'encyclopedia/index.html',{
+            "heading":'Search Results',
+            "entries":results,
+            "query":query,
+        })
+
+    
