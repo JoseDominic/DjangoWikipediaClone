4f8384e9b803c32d9003559d176a61b95530d155
josedominic <josudominic007@gmail.com>
2020-07-13 14:11:20 +0530

    all routes complete

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -7,6 +7,14 @@ from . import util
 
 from markdown2 import markdown
 import re
+import random as rn
+
+class EntryForm(forms.Form):
+    title = forms.CharField(label='Title')
+    entry = forms.CharField(widget=forms.Textarea)
+
+class EditForm(forms.Form):
+    entry = forms.CharField(widget=forms.Textarea)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -40,4 +48,51 @@ def search(request):
             "query":query,
         })
 
+def new(request):
+    if request.method=='POST':
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            entry = form.cleaned_data['entry']
+            title = form.cleaned_data['title']
+            entries = util.list_entries()
+            if title in entries:
+                return render(request,'encyclopedia/new.html',{
+                    "form":form,
+                    "error":'Title already exists!Please give another title!'
+                })
+            util.save_entry(title,entry)
+            return HttpResponseRedirect(reverse('show_wiki',kwargs={"title":title}))
+        else:
+            return render(request,'encyclopedia/new.html',{
+                'form':form
+            })
+    return render(request,'encyclopedia/new.html',{
+        "form":EntryForm()
+    })
+
+def edit(request,title):
+    if request.method =='POST':
+        form = EditForm(request.POST)
+        if form.is_valid():
+            entry = form.cleaned_data['entry']
+            util.save_entry(title,entry)
+            return HttpResponseRedirect(reverse('show_wiki',kwargs={'title':title}))
+        else:
+            return render(request,'encyclopedia/edit.html',{
+                'title':title,
+                'form':form
+            })
+    entry = util.get_entry(title)
+    form = EditForm({"entry":entry})
+    return render(request,'encyclopedia/edit.html',{
+        'title':title,
+        'form':form
+    })
+
+#view that returns a random wiki page
+def random(request):
+    entries = util.list_entries()
+    n = rn.randint(0,len(entries)-1)
+    title = entries[n]
+    return HttpResponseRedirect(reverse('show_wiki',kwargs={'title':title}))
     
